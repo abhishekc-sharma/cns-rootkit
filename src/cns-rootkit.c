24065e4b9edeba0b192dd8c87f0a74509112f447
Abhishek Sharma <Abhishek Sharma>
2017-04-22 08:51:54 +0530

    Modified log messages to be easier to recognize

--- src/cns-rootkit.c
+++ src/cns-rootkit.c
@@ -7,12 +7,12 @@ MODULE_AUTHOR("SAV");
 
 
 static int cns_rootkit_init(void) {
-  printk(KERN_INFO "Init cns-rootkit\n");
+  printk(KERN_INFO "cns-rootkit: Init\n");
   return 0;
 }
 
 static void cns_rootkit_exit(void) {
-  printk(KERN_INFO "Exit cns-rootkit\n");
+  printk(KERN_INFO "cns-rootkit: Exit\n");
 }
 
 module_init(cns_rootkit_init);
